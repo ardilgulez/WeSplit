1e2e94c276e3d0998790e8cdc79467a6195df1ea
Kemal Ardıl Gülez <kemal.guelez@styla.com>
2022-09-13 20:48:23 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Kemal Ardil Gülez on 13.09.22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
